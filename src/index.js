633a1e4df7a75b11feebcbf298f44feb05691462
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-05-10 15:54:54 +0530

    added components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./Components/SearchBar";
+import ShowWeather from "./Components/ShowWeather";
 
 import "./styles.css";
 
 function App() {
-  return <div className="App">nmncmn</div>;
+  return (
+    <div>
+      <SearchBar />
+      <ShowWeather />
+    </div>
+  );
 }
+
+// let weatherUrl =
+//   "http://api.openweathermap.org/data/2.5/weather?q=jabalpur&APPID=966604145f9141f0872bbd24cea3a1f2";
+
+// fetch(weatherUrl)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
